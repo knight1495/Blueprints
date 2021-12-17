b19926bd7961a10251bc94bbdee390ee1e016af5
knight1495 <tomas.figueroa@sarria.salesians.cat>
2021-12-17 19:55:44 +0100

    Agregar archivos de proyecto.

---
+++ BlueprintsInfoDC/frmBlueprints.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace BlueprintsInfoDC
+{
+    partial class frmBlueprints
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBlueprints));
+            this.pnlGeneralinfo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitol = new System.Windows.Forms.Label();
+            this.picGeneral = new System.Windows.Forms.Panel();
+            this.labelTransparent2 = new ControlsDC.LabelTransparent();
+            this.labelTransparent1 = new ControlsDC.LabelTransparent();
+            this.labelTransparent3 = new ControlsDC.LabelTransparent();
+            this.lblZoomInfo = new System.Windows.Forms.Label();
+            this.pnlGeneralinfo.SuspendLayout();
+            this.picGeneral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlGeneralinfo
+            // 
+            this.pnlGeneralinfo.Controls.Add(this.label1);
+            this.pnlGeneralinfo.Controls.Add(this.lblTitol);
+            this.pnlGeneralinfo.Controls.Add(this.picGeneral);
+            this.pnlGeneralinfo.Controls.Add(this.lblZoomInfo);
+            this.pnlGeneralinfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGeneralinfo.ForeColor = System.Drawing.Color.Yellow;
+            this.pnlGeneralinfo.Location = new System.Drawing.Point(0, 0);
+            this.pnlGeneralinfo.Name = "pnlGeneralinfo";
+            this.pnlGeneralinfo.Size = new System.Drawing.Size(1110, 724);
+            this.pnlGeneralinfo.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(50, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 25);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "StarKiller Technical Data";
+            // 
+            // lblTitol
+            // 
+            this.lblTitol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitol.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTitol.Location = new System.Drawing.Point(519, 484);
+            this.lblTitol.Name = "lblTitol";
+            this.lblTitol.Size = new System.Drawing.Size(557, 39);
+            this.lblTitol.TabIndex = 23;
+            this.lblTitol.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picGeneral
+            // 
+            this.picGeneral.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picGeneral.BackgroundImage")));
+            this.picGeneral.Controls.Add(this.labelTransparent2);
+            this.picGeneral.Controls.Add(this.labelTransparent1);
+            this.picGeneral.Controls.Add(this.labelTransparent3);
+            this.picGeneral.Location = new System.Drawing.Point(23, 72);
+            this.picGeneral.Name = "picGeneral";
+            this.picGeneral.Size = new System.Drawing.Size(350, 300);
+            this.picGeneral.TabIndex = 22;
+            // 
+            // labelTransparent2
+            // 
+            this.labelTransparent2.BackColor = System.Drawing.Color.Crimson;
+            this.labelTransparent2.Caption = "click here";
+            this.labelTransparent2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelTransparent2.DimmedColor = System.Drawing.Color.Yellow;
+            this.labelTransparent2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTransparent2.ForeColor = System.Drawing.Color.Transparent;
+            this.labelTransparent2.Location = new System.Drawing.Point(242, 58);
+            this.labelTransparent2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.labelTransparent2.Name = "labelTransparent2";
+            this.labelTransparent2.Opacity = 1;
+            this.labelTransparent2.Radius = 0;
+            this.labelTransparent2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelTransparent2.Size = new System.Drawing.Size(96, 81);
+            this.labelTransparent2.TabIndex = 25;
+            this.labelTransparent2.Tag = "1";
+            // 
+            // labelTransparent1
+            // 
+            this.labelTransparent1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.labelTransparent1.Caption = "Click";
+            this.labelTransparent1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelTransparent1.DimmedColor = System.Drawing.Color.Yellow;
+            this.labelTransparent1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTransparent1.ForeColor = System.Drawing.Color.Transparent;
+            this.labelTransparent1.Location = new System.Drawing.Point(1, 1);
+            this.labelTransparent1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.labelTransparent1.Name = "labelTransparent1";
+            this.labelTransparent1.Opacity = 1;
+            this.labelTransparent1.Radius = 0;
+            this.labelTransparent1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelTransparent1.Size = new System.Drawing.Size(52, 48);
+            this.labelTransparent1.TabIndex = 24;
+            this.labelTransparent1.Tag = "3";
+            // 
+            // labelTransparent3
+            // 
+            this.labelTransparent3.BackColor = System.Drawing.Color.OrangeRed;
+            this.labelTransparent3.Caption = "click here";
+            this.labelTransparent3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelTransparent3.DimmedColor = System.Drawing.Color.Yellow;
+            this.labelTransparent3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTransparent3.ForeColor = System.Drawing.Color.Transparent;
+            this.labelTransparent3.Location = new System.Drawing.Point(119, 94);
+            this.labelTransparent3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.labelTransparent3.Name = "labelTransparent3";
+            this.labelTransparent3.Opacity = 1;
+            this.labelTransparent3.Radius = 0;
+            this.labelTransparent3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelTransparent3.Size = new System.Drawing.Size(115, 107);
+            this.labelTransparent3.TabIndex = 23;
+            this.labelTransparent3.Tag = "2";
+            // 
+            // lblZoomInfo
+            // 
+            this.lblZoomInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZoomInfo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblZoomInfo.Location = new System.Drawing.Point(27, 385);
+            this.lblZoomInfo.Name = "lblZoomInfo";
+            this.lblZoomInfo.Size = new System.Drawing.Size(334, 80);
+            this.lblZoomInfo.TabIndex = 17;
+            this.lblZoomInfo.Text = "Check in the image for detailed area  and click for enlarge it";
+            this.lblZoomInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmBlueprints
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1110, 724);
+            this.Controls.Add(this.pnlGeneralinfo);
+            this.Name = "frmBlueprints";
+            this.Text = "frmBlueprints";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlGeneralinfo.ResumeLayout(false);
+            this.pnlGeneralinfo.PerformLayout();
+            this.picGeneral.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlGeneralinfo;
+        private System.Windows.Forms.Label lblZoomInfo;
+        private System.Windows.Forms.Panel picGeneral;
+        private ControlsDC.LabelTransparent labelTransparent3;
+        private ControlsDC.LabelTransparent labelTransparent2;
+        private ControlsDC.LabelTransparent labelTransparent1;
+        private System.Windows.Forms.Label lblTitol;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BlueprintsInfoDC/frmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.Configuration;
+using System.Net.Configuration;
+
+namespace BlueprintsInfoDC
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmBlueprints frm = new frmBlueprints();
+            frm.Show();
+        }
+    }
+}
