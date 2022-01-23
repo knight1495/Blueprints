17f6c3aae66d7c7ae31e2399656a63f8db8d6907
knight1495 <tomas.figueroa@sarria.salesians.cat>
2022-01-23 23:03:10 +0100

    Casi terminado, falta ver pdf y cargar mp4

--- BlueprintsInfoDC/starships.Designer.cs
+++ BlueprintsInfoDC/starships.Designer.cs
@@ -29,151 +29,191 @@ namespace BlueprintsInfoDC
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
             this.panel1 = new System.Windows.Forms.Panel();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lvListSpaceships = new System.Windows.Forms.ListView();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.wbPdf = new System.Windows.Forms.WebBrowser();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pbSelected = new System.Windows.Forms.PictureBox();
+            this.pbFront = new System.Windows.Forms.PictureBox();
+            this.pbSide = new System.Windows.Forms.PictureBox();
+            this.pbTop = new System.Windows.Forms.PictureBox();
+            this.pbRear = new System.Windows.Forms.PictureBox();
+            this.pb360 = new System.Windows.Forms.PictureBox();
+            this.lblNave = new System.Windows.Forms.Label();
+            this.dgvDatosnave = new System.Windows.Forms.DataGridView();
+            this.Data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbNaves = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelected)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFront)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb360)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosnave)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(176, 49);
+            this.panel1.Controls.Add(this.wbPdf);
+            this.panel1.Location = new System.Drawing.Point(211, 49);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(311, 270);
             this.panel1.TabIndex = 1;
             // 
-            // tableLayoutPanel1
+            // wbPdf
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(176, 342);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(311, 94);
-            this.tableLayoutPanel1.TabIndex = 2;
+            this.wbPdf.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbPdf.Location = new System.Drawing.Point(0, 0);
+            this.wbPdf.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbPdf.Name = "wbPdf";
+            this.wbPdf.Size = new System.Drawing.Size(311, 270);
+            this.wbPdf.TabIndex = 0;
             // 
-            // label1
+            // lblTitulo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(728, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(640, 49);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(278, 20);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Spaceship technical Information";
             // 
-            // pictureBox1
+            // pbSelected
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(630, 103);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(234, 188);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.pbSelected.Location = new System.Drawing.Point(670, 105);
+            this.pbSelected.Name = "pbSelected";
+            this.pbSelected.Size = new System.Drawing.Size(234, 188);
+            this.pbSelected.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSelected.TabIndex = 4;
+            this.pbSelected.TabStop = false;
             // 
-            // pictureBox2
+            // pbFront
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(551, 321);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(62, 59);
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
+            this.pbFront.Location = new System.Drawing.Point(630, 321);
+            this.pbFront.Name = "pbFront";
+            this.pbFront.Size = new System.Drawing.Size(62, 59);
+            this.pbFront.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFront.TabIndex = 5;
+            this.pbFront.TabStop = false;
+            this.pbFront.Click += new System.EventHandler(this.pbFront_Click);
             // 
-            // pictureBox3
+            // pbSide
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(630, 321);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(62, 59);
-            this.pictureBox3.TabIndex = 6;
-            this.pictureBox3.TabStop = false;
+            this.pbSide.Location = new System.Drawing.Point(698, 321);
+            this.pbSide.Name = "pbSide";
+            this.pbSide.Size = new System.Drawing.Size(62, 59);
+            this.pbSide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSide.TabIndex = 6;
+            this.pbSide.TabStop = false;
+            this.pbSide.Click += new System.EventHandler(this.pbSide_Click);
             // 
-            // pictureBox4
+            // pbTop
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(712, 321);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(62, 59);
-            this.pictureBox4.TabIndex = 7;
-            this.pictureBox4.TabStop = false;
+            this.pbTop.Location = new System.Drawing.Point(766, 321);
+            this.pbTop.Name = "pbTop";
+            this.pbTop.Size = new System.Drawing.Size(62, 59);
+            this.pbTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTop.TabIndex = 7;
+            this.pbTop.TabStop = false;
+            this.pbTop.Click += new System.EventHandler(this.pbTop_Click);
             // 
-            // pictureBox5
+            // pbRear
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(790, 321);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(62, 59);
-            this.pictureBox5.TabIndex = 8;
-            this.pictureBox5.TabStop = false;
+            this.pbRear.Location = new System.Drawing.Point(834, 321);
+            this.pbRear.Name = "pbRear";
+            this.pbRear.Size = new System.Drawing.Size(62, 59);
+            this.pbRear.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbRear.TabIndex = 8;
+            this.pbRear.TabStop = false;
+            this.pbRear.Click += new System.EventHandler(this.pbRear_Click);
             // 
-            // pictureBox6
+            // pb360
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(867, 321);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(62, 59);
-            this.pictureBox6.TabIndex = 9;
-            this.pictureBox6.TabStop = false;
+            this.pb360.Location = new System.Drawing.Point(902, 321);
+            this.pb360.Name = "pb360";
+            this.pb360.Size = new System.Drawing.Size(62, 59);
+            this.pb360.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb360.TabIndex = 9;
+            this.pb360.TabStop = false;
+            this.pb360.Click += new System.EventHandler(this.pb360_Click);
             // 
-            // label2
+            // lblNave
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(728, 409);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "label2";
+            this.lblNave.AutoSize = true;
+            this.lblNave.Location = new System.Drawing.Point(728, 409);
+            this.lblNave.Name = "lblNave";
+            this.lblNave.Size = new System.Drawing.Size(0, 17);
+            this.lblNave.TabIndex = 10;
             // 
-            // lvListSpaceships
+            // dgvDatosnave
             // 
-            this.lvListSpaceships.HideSelection = false;
-            this.lvListSpaceships.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.lvListSpaceships.Location = new System.Drawing.Point(12, 49);
-            this.lvListSpaceships.Name = "lvListSpaceships";
-            this.lvListSpaceships.Size = new System.Drawing.Size(121, 156);
-            this.lvListSpaceships.TabIndex = 11;
-            this.lvListSpaceships.UseCompatibleStateImageBehavior = false;
-            this.lvListSpaceships.View = System.Windows.Forms.View.List;
+            this.dgvDatosnave.AllowUserToOrderColumns = true;
+            this.dgvDatosnave.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatosnave.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Data,
+            this.Value});
+            this.dgvDatosnave.Location = new System.Drawing.Point(220, 325);
+            this.dgvDatosnave.Name = "dgvDatosnave";
+            this.dgvDatosnave.RowHeadersWidth = 51;
+            this.dgvDatosnave.RowTemplate.Height = 24;
+            this.dgvDatosnave.Size = new System.Drawing.Size(302, 150);
+            this.dgvDatosnave.TabIndex = 12;
+            // 
+            // Data
+            // 
+            this.Data.HeaderText = "Data";
+            this.Data.MinimumWidth = 6;
+            this.Data.Name = "Data";
+            this.Data.ReadOnly = true;
+            this.Data.Width = 125;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.MinimumWidth = 6;
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            this.Value.Width = 125;
+            // 
+            // lbNaves
+            // 
+            this.lbNaves.FormattingEnabled = true;
+            this.lbNaves.ItemHeight = 16;
+            this.lbNaves.Location = new System.Drawing.Point(12, 49);
+            this.lbNaves.Name = "lbNaves";
+            this.lbNaves.Size = new System.Drawing.Size(137, 244);
+            this.lbNaves.TabIndex = 13;
+            this.lbNaves.SelectedIndexChanged += new System.EventHandler(this.lbNaves_SelectedIndexChanged);
             // 
             // starships
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(964, 503);
-            this.Controls.Add(this.lvListSpaceships);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(1069, 548);
+            this.Controls.Add(this.lbNaves);
+            this.Controls.Add(this.dgvDatosnave);
+            this.Controls.Add(this.lblNave);
+            this.Controls.Add(this.pb360);
+            this.Controls.Add(this.pbRear);
+            this.Controls.Add(this.pbTop);
+            this.Controls.Add(this.pbSide);
+            this.Controls.Add(this.pbFront);
+            this.Controls.Add(this.pbSelected);
+            this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.panel1);
             this.Name = "starships";
             this.Text = "Starship";
             this.Load += new System.EventHandler(this.starships_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelected)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFront)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb360)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosnave)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,15 +221,18 @@ namespace BlueprintsInfoDC
 
         #endregion
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListView lvListSpaceships;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox pbSelected;
+        private System.Windows.Forms.PictureBox pbFront;
+        private System.Windows.Forms.PictureBox pbSide;
+        private System.Windows.Forms.PictureBox pbTop;
+        private System.Windows.Forms.PictureBox pbRear;
+        private System.Windows.Forms.PictureBox pb360;
+        private System.Windows.Forms.Label lblNave;
+        private System.Windows.Forms.DataGridView dgvDatosnave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.ListBox lbNaves;
+        private System.Windows.Forms.WebBrowser wbPdf;
     }
 }
--- BlueprintsInfoDC/starships.cs
+++ BlueprintsInfoDC/starships.cs
@@ -13,6 +13,10 @@ namespace BlueprintsInfoDC
 {
     public partial class starships : Form
     {
+
+        string filePath = Application.StartupPath + "\\files\\info.xml";
+        XmlDocument doc = new XmlDocument();
+        
         public starships()
         {
             InitializeComponent();
@@ -20,14 +24,151 @@ namespace BlueprintsInfoDC
 
         private void starships_Load(object sender, EventArgs e)
         {
-            string filePath = Application.StartupPath + "\\files\\info.xml";
-            XmlDocument doc = new XmlDocument();
+
+            
             doc.Load(filePath);
-            XmlNodeList details = doc.SelectNodes("//TechnicalInfo//[title]");
-            foreach (XmlNode nodo in details)
+            ListViewGroup naves = new ListViewGroup("Naves");
+            XmlNodeList detailsTitle = doc.SelectNodes("//InfoOptions//textOption");
+            lbNaves.Items.Clear();
+            foreach (XmlNode nodo in detailsTitle)
             {
-                lvListSpaceships.Items.Add(nodo.InnerText);   
+
+                lbNaves.Items.Add(nodo.InnerText);
             }
+
+            //Seleccionar foto y meterla
+            //XmlNodeList detailsFoto = doc.SelectNodes("//TechnicalInfo//Blueprint");
+            //ListView
+            //foreach (XmlNode nodo in detailsFoto)
+            //{
+                
+                
+            //}
+
+            
+
+        }
+
+        
+
+        private void añadirInfoDesdeXML(string clave)
+        {
+            doc.Load(filePath);
+            XmlNode details = doc.SelectSingleNode("//TechnicalInfo//InfoDetails//InfoDetail[idInfoDetail ='" + clave + "']//Data" );
+            
+            foreach(XmlNode nodo in details)
+            {
+                dgvDatosnave.Rows.Add(nodo.Name.ToString(), nodo.InnerText.ToString());
+            }
+
+        }
+
+        private void lbNaves_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string nave;
+            nave = lbNaves.SelectedIndex.ToString();
+            dgvDatosnave.Rows.Clear();
+            añadirInfoDesdeXML(nave);
+            lblNave.Text = lbNaves.SelectedItem.ToString();
+
+            pbFront.Image = Image.FromFile(Application.StartupPath+"//images//"+lbNaves.SelectedItem+"//"+fotoFront(nave));
+            pb360.Image = Image.FromFile(Application.StartupPath + "//images//" + lbNaves.SelectedItem + "//" + foto360(nave));
+            pbRear.Image = Image.FromFile(Application.StartupPath + "//images//" + lbNaves.SelectedItem + "//" + fotoRear(nave));
+            pbSide.Image = Image.FromFile(Application.StartupPath + "//images//" + lbNaves.SelectedItem + "//" + fotoSide(nave));
+            pbTop.Image = Image.FromFile(Application.StartupPath + "//images//" + lbNaves.SelectedItem + "//" + fotoTop(nave));
+
+            //wbPdf.Url = Application.StartupPath + "//images//" + lbNaves.SelectedItem + "//" + pillapdf(nave);
+
+
+        }
+
+        private string pillapdf(string nave)
+        {
+            string pdf;
+            XmlNode nodopdf = doc.SelectSingleNode("//TechnicalInfo//InfoDetails//InfoDetail[idInfoDetail ='" + nave + "']//pdfFile");
+
+            pdf = nodopdf.InnerText;
+            return pdf;
+        }
+
+        private string fotoFront(string nave)
+        {
+            string foto;
+            XmlNode frontFoto = doc.SelectSingleNode("//TechnicalInfo//InfoDetails//InfoDetail[idInfoDetail ='" + nave + "']//FrontView");
+            
+            foto = frontFoto.InnerText;
+            
+
+            return foto;
+        }
+
+        private string fotoTop(string nave)
+        {
+            string foto;
+            XmlNode topFoto = doc.SelectSingleNode("//TechnicalInfo//InfoDetails//InfoDetail[idInfoDetail ='" + nave + "']//TopView");
+
+            foto = topFoto.InnerText;
+
+
+            return foto;
+        }
+
+        private string foto360(string nave)
+        {
+            string foto;
+            XmlNode frontFoto = doc.SelectSingleNode("//TechnicalInfo//InfoDetails//InfoDetail[idInfoDetail ='" + nave + "']//View360");
+
+            foto = frontFoto.InnerText;
+
+
+            return foto;
+        }
+
+        private string fotoSide(string nave)
+        {
+            string foto;
+            XmlNode frontFoto = doc.SelectSingleNode("//TechnicalInfo//InfoDetails//InfoDetail[idInfoDetail ='" + nave + "']//SideView");
+
+            foto = frontFoto.InnerText;
+
+
+            return foto;
+        }
+
+        private string fotoRear(string nave)
+        {
+            string foto;
+            XmlNode frontFoto = doc.SelectSingleNode("//TechnicalInfo//InfoDetails//InfoDetail[idInfoDetail ='" + nave + "']//RearView");
+
+            foto = frontFoto.InnerText;
+
+
+            return foto;
+        }
+
+        private void pbFront_Click(object sender, EventArgs e)
+        {
+            pbSelected.Image = pbFront.Image;
+        }
+
+        private void pbSide_Click(object sender, EventArgs e)
+        {
+            pbSelected.Image = pbSide.Image;
+        }
+
+        private void pbTop_Click(object sender, EventArgs e)
+        {
+            pbSelected.Image = pbTop.Image;
+        }
+
+        private void pbRear_Click(object sender, EventArgs e)
+        {
+            pbSelected.Image = pbRear.Image;
+        }
+
+        private void pb360_Click(object sender, EventArgs e)
+        {
+            pbSelected.Image = pb360.Image;
         }
     }
 }
